d59815cb827db9538d47cb4847156431ab0d4680
Ale00-1 <ale2025pc@gmail.com>
2025-07-23 04:59:56 -0300

    Add RandomIntGenerator function and numberToChoice function

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 console.log('Hello Player')
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomInteger = getRandomInt(1, 10); // Generates a random integer between 1 and 10 (inclusive)
+console.log(randomInteger);
 function checkChoice(x){
          if (x == 'ROCK' || x == 'PAPER' || x == 'SCISSORS'){
             console.log('bueno')
@@ -11,18 +19,32 @@ function checkChoice(x){
                 return valido
             }
         }
+function numberToChoice(x){
+    if (x == 1){
+        botChoice = 'ROCK'
+        return botChoice
+    if (x == 2){
+        botChoice = 'PAPER'
+        return botChoice
+    if (x == 3){
+        botChoice = 'SCISSORS'
+        return botChoice
+    }
+    }
+    }
+}
+
 var valido = false
 var userChoice = ''
 
 if (valido == false){
     if (userChoice == ''){
         userChoice = prompt('Please choose between Rock, Paper or Scissors');
-        console.log(userChoice)
         userChoice = userChoice.toUpperCase();
-        console.log(userChoice);
     }
     checkChoice(userChoice);
         }
-if (valido == true){
 
-}
+var botPick = getRandomInt(1,3)
+numberToChoice(botPick)
+
